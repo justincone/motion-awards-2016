ea8df66fcd0cb9af95b4deb9cedcf9f7378c22a2
justincone <me@justincone.com>
2017-02-27 16:08:25 -0600

    jQuery to insert headings before each category

--- custom-script.js
+++ custom-script.js
@@ -1,3 +1,10 @@
+/* This code is copy and pasted into the sidebar source for the public galery page */
 $( document ).ready(function() {
     console.log( "ready!" );
+    var bns = $( "span:contains('Best New Studio')" ).first().closest("div.gallery_col");
+    bns.css( "clear", "both" );
+    bns.before("<h2 style='display: block; clear: both; width: 100%;'>Best New Studio</h2>");
+    var bnt = $( "span:contains('Best New Talent')" ).first().closest("div.gallery_col");
+    bnt.css( "clear", "both" );
+    bnt.before("<h2 style='display: block; clear: both; width: 100%;'>Best New Talent</h2>");
 });
